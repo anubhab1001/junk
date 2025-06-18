e70e6e35880897fb724c8383b75eeba2451701e4
ANUBHAB DE <161861837+anubhab1001@users.noreply.github.com>
2025-06-18 14:48:50 +0530

    day 1

---
+++ palindrome.cpp
@@ -0,0 +1,25 @@
+//Given an integer x, return true if x is a palindrome, and false otherwise.
+//Beware of overflow when you reverse the integer.
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        bool result = false ;
+        long int sum = 0 ; // to reduce overflow error 
+        int temp = x ;
+        if(x < 0){
+            result = false;
+        }
+        else{
+            while(temp > 0){
+                int rem = temp % 10 ;
+                sum = (sum * 10) + rem ;
+                temp = temp / 10;
+             }
+            if (x == sum ){
+                result = true ;
+                }
+        }
+        return result;
+    }
+};
