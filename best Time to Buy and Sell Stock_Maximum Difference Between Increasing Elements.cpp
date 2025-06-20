b549443d631152b4281a41b0a3e23fcf2a56f44b
ANUBHAB DE <161861837+anubhab1001@users.noreply.github.com>
2025-06-20 13:29:23 +0530

    2016 , 121

--- best Time to Buy and Sell Stock_Maximum Difference Between Increasing Elements.cpp
+++ best Time to Buy and Sell Stock_Maximum Difference Between Increasing Elements.cpp
@@ -1,4 +1,22 @@
 /*
+---------type 1----------
+Best Time to Buy and Sell Stock
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+Constraints:
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+----------type 2-----------
 Given a 0-indexed integer array nums of size n, find the maximum difference between nums[i] and nums[j] 
 (i.e., nums[j] - nums[i]), such that 0 <= i < j < n and nums[i] < nums[j].
 Return the maximum difference. If no such i and j exists, return -1.
@@ -41,7 +59,7 @@ public:
     }
 };
 
-// O(n^2) O(n^2) solution 
+// O(n^2) O(n^2) solution // Memory Limit Exceeded 
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
